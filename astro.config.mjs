2f2f68497c9ad5edebe6762ce21d72753dfc1830
nakolas-024 <nicolasvera39205@gmail.com>
2025-09-26 10:24:06 -0300

    dospatitas terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon"; // <-- 1. AÑADE ESTA LÍNEA
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 2. AÑADE ESTA SECCIÓN COMPLETA
+  integrations: [
+    icon() 
+  ]
+});
---
+++ src/data/products.js
@@ -0,0 +1,864 @@
+export const products = [
+  {
+    id: "vet01",
+    name: "VET01",
+    category: "veterinarios",
+    images: ["/images/VET01.png", "/images/VET01a.png"]
+  },
+  {
+    id: "vet02",
+    name: "VET02",
+    category: "veterinarios",
+    images: ["/images/VET02.png", "/images/VET02a.png"]
+  },
+  {
+    id: "vet03",
+    name: "VET03",
+    category: "veterinarios",
+    images: ["/images/VET03.jpg", "/images/VET03a.jpg"]
+  },
+  {
+    id: "vet04",
+    name: "VET04",
+    category: "veterinarios",
+    images: ["/images/VET04.jpg", "/images/VET04a.jpg"]
+  },
+  {
+    id: "vet05",
+    name: "VET05",
+    category: "veterinarios",
+    images: ["/images/VET05.jpg", "/images/VET05a.jpg"]
+  },
+  {
+    id: "vet06",
+    name: "VET06",
+    category: "veterinarios",
+    images: ["/images/VET06.jpg", "/images/VET06a.jpg"]
+  },
+  {
+    id: "vet07",
+    name: "VET07",
+    category: "veterinarios",
+    images: ["/images/VET07.jpg", "/images/VET07a.jpg"]
+  },
+  {
+    id: "vet08",
+    name: "VET08",
+    category: "veterinarios",
+    images: ["/images/VET08.jpg", "/images/VET08a.jpg"]
+  },
+  {
+    id: "vet09",
+    name: "VET09",
+    category: "veterinarios",
+    images: ["/images/VET09.jpg", "/images/VET09a.jpg"]
+  },
+  {
+    id: "vet10",
+    name: "VET10",
+    category: "veterinarios",
+    images: ["/images/VET10.jpg", "/images/VET10a.jpg"]
+  },
+  {
+    id: "vet11",
+    name: "VET11",
+    category: "veterinarios",
+    images: ["/images/VET11.jpg", "/images/VET11a.jpg"]
+  },
+  {
+    id: "vet12",
+    name: "VET12",
+    category: "veterinarios",
+    images: ["/images/VET12.jpg", "/images/VET12a.jpg"]
+  },
+  {
+    id: "vet13",
+    name: "VET13",
+    category: "veterinarios",
+    images: ["/images/VET13.jpg", "/images/VET13a.jpg"]
+  },
+  {
+    id: "vet14",
+    name: "VET14",
+    category: "veterinarios",
+    images: ["/images/VET14.jpg", "/images/VET14a.jpg"]
+  },
+  {
+    id: "vet15",
+    name: "VET15",
+    category: "veterinarios",
+    images: ["/images/VET15.jpg", "/images/VET15a.jpg"]
+  },
+  {
+    id: "vet16",
+    name: "VET16",
+    category: "veterinarios",
+    images: ["/images/VET16.jpg", "/images/VET16a.jpg"]
+  },
+  {
+    id: "vet17",
+    name: "VET17",
+    category: "veterinarios",
+    images: ["/images/VET17.jpg", "/images/VET17a.jpg"]
+  },
+  {
+    id: "vet18",
+    name: "VET18",
+    category: "veterinarios",
+    images: ["/images/VET18.jpg", "/images/VET18a.jpg"]
+  },
+  {
+    id: "vet19",
+    name: "VET19",
+    category: "veterinarios",
+    images: ["/images/VET19.jpg", "/images/VET19a.jpg"]
+  },
+  {
+    id: "vet20",
+    name: "VET20",
+    category: "veterinarios",
+    images: ["/images/VET20.jpg", "/images/VET20a.jpg"]
+  },
+  {
+    id: "vet21",
+    name: "VET21",
+    category: "veterinarios",
+    images: ["/images/VET21.jpg", "/images/VET21a.jpg"]
+  },
+  {
+    id: "vet22",
+    name: "VET22",
+    category: "veterinarios",
+    images: ["/images/VET22.jpg", "/images/VET22a.jpg"]
+  },
+  {
+    id: "vet23",
+    name: "VET23",
+    category: "veterinarios",
+    images: ["/images/VET23.jpg", "/images/VET23a.jpg"]
+  },
+  {
+    id: "vet24",
+    name: "VET24",
+    category: "veterinarios",
+    images: ["/images/VET24.jpg", "/images/VET24a.jpg"]
+  },
+  {
+    id: "vet25",
+    name: "VET25",
+    category: "veterinarios",
+    images: ["/images/VET25.jpg", "/images/VET25a.jpg"]
+  },
+  {
+    id: "vet26",
+    name: "VET26",
+    category: "veterinarios",
+    images: ["/images/VET26.jpg", "/images/VET26a.jpg"]
+  },
+  {
+    id: "una01",
+    name: "UNA01",
+    category: "niñas",
+    images: ["/images/UNA01.png", "/images/UNA01a.png"]
+  },
+  {
+    id: "una02",
+    name: "UNA02",
+    category: "niñas",
+    images: ["/images/UNA02.png", "/images/UNA02a.png"]
+  },
+  {
+    id: "una03",
+    name: "UNA03",
+    category: "niñas",
+    images: ["/images/UNA03.png", "/images/UNA03a.png"]
+  },
+  {
+    id: "una04",
+    name: "UNA04",
+    category: "niñas",
+    images: ["/images/UNA04.jpg", "/images/UNA04a.jpg"]
+  },
+  {
+    id: "una05",
+    name: "UNA05",
+    category: "niñas",
+    images: ["/images/UNA05.jpg", "/images/UNA05a.jpg"]
+  },
+  {
+    id: "una06",
+    name: "UNA06",
+    category: "niñas",
+    images: ["/images/UNA06.jpg", "/images/UNA06a.jpg"]
+  },
+  {
+    id: "sire01",
+    name: "SIRE01",
+    category: "niñas",
+    images: ["/images/SIRE01.jpg", "/images/SIRE01a.jpg"]
+  },
+  {
+    id: "sire02",
+    name: "SIRE02",
+    category: "niñas",
+    images: ["/images/SIRE02.jpg", "/images/SIRE02a.jpg"]
+  },
+  {
+    id: "prina01",
+    name: "PRINA01",
+    category: "niñas",
+    images: ["/images/PRINA01.png", "/images/PRINA01a.png"]
+  },
+  {
+    id: "prina02",
+    name: "PRINA02",
+    category: "niñas",
+    images: ["/images/PRINA02.png", "/images/PRINA02a.png"]
+  },
+  {
+    id: "prina03",
+    name: "PRINA03",
+    category: "niñas",
+    images: ["/images/PRINA03.png", "/images/PRINA03a.png"]
+  },
+  {
+    id: "pep01",
+    name: "PEP01",
+    category: "niñas",
+    images: ["/images/PEP01.png", "/images/PEP01a.png"]
+  },
+  {
+    id: "pep02",
+    name: "PEP02",
+    category: "niñas",
+    images: ["/images/PEP02.jpg", "/images/PEP02a.jpg"]
+  },
+  {
+    id: "min01",
+    name: "MIN01",
+    category: "niñas",
+    images: ["/images/MIN01.png", "/images/MIN01a.png"]
+  },
+  {
+    id: "min02",
+    name: "MIN02",
+    category: "niñas",
+    images: ["/images/MIN02.png", "/images/MIN02a.png"]
+  },
+  {
+    id: "min03",
+    name: "MIN03",
+    category: "niñas",
+    images: ["/images/MIN03.png", "/images/MIN03a.png"]
+  },
+  {
+    id: "min04",
+    name: "MIN04",
+    category: "niñas",
+    images: ["/images/MIN04.png", "/images/MIN04a.png"]
+  },
+  {
+    id: "min05",
+    name: "MIN05",
+    category: "niñas",
+    images: ["/images/MIN05.png", "/images/MIN05a.png"]
+  },
+  {
+    id: "min06",
+    name: "MIN06",
+    category: "niñas",
+    images: ["/images/MIN06.png", "/images/MIN06a.png"]
+  },
+  {
+    id: "min07",
+    name: "MIN07",
+    category: "niñas",
+    images: ["/images/MIN07.png", "/images/MIN07a.png"]
+  },
+  {
+    id: "min08",
+    name: "MIN08",
+    category: "niñas",
+    images: ["/images/MIN08.png", "/images/MIN08a.png"]
+  },
+  {
+    id: "min09",
+    name: "MIN09",
+    category: "niñas",
+    images: ["/images/MIN09.png", "/images/MIN09a.png"]
+  },
+  {
+    id: "min10",
+    name: "MIN10",
+    category: "niñas",
+    images: ["/images/MIN10.png", "/images/MIN10a.png"]
+  },
+  {
+    id: "min11",
+    name: "MIN11",
+    category: "niñas",
+    images: ["/images/MIN11.png", "/images/MIN11a.png"]
+  },
+  {
+    id: "min12",
+    name: "MIN12",
+    category: "niñas",
+    images: ["/images/MIN12.png", "/images/MIN12a.png"]
+  },
+  {
+    id: "min13",
+    name: "MIN13",
+    category: "niñas",
+    images: ["/images/MIN13.jpg", "/images/MIN13a.jpg"]
+  },
+  {
+    id: "min14",
+    name: "MIN14",
+    category: "niñas",
+    images: ["/images/MIN14.jpg", "/images/MIN14a.jpg"]
+  },
+  {
+    id: "min15",
+    name: "MIN15",
+    category: "niñas",
+    images: ["/images/MIN15.jpg", "/images/MIN15a.jpg"]
+  },
+  {
+    id: "min16",
+    name: "MIN16",
+    category: "niñas",
+    images: ["/images/MIN16.jpg", "/images/MIN16a.jpg"]
+  },
+  {
+    id: "min17",
+    name: "MIN17",
+    category: "niñas",
+    images: ["/images/MIN17.jpg", "/images/MIN17a.jpg"]
+  },
+  {
+    id: "min18",
+    name: "MIN18",
+    category: "niñas",
+    images: ["/images/MIN18.jpg", "/images/MIN18a.jpg"]
+  },
+  {
+    id: "min19",
+    name: "MIN19",
+    category: "niñas",
+    images: ["/images/MIN19.jpg", "/images/MIN19a.jpg"]
+  },
+  {
+    id: "mara01",
+    name: "MARA01",
+    category: "niñas",
+    images: ["/images/MARA01.jpg", "/images/MARA01a.jpg"]
+  },
+  {
+    id: "mara02",
+    name: "MARA02",
+    category: "niñas",
+    images: ["/images/MARA02.jpg", "/images/MARA02a.jpg"]
+  },
+  {
+    id: "lad01",
+    name: "LAD01",
+    category: "niñas",
+    images: ["/images/LAD01.png", "/images/LAD01a.png"]
+  },
+  {
+    id: "lad02",
+    name: "LAD02",
+    category: "niñas",
+    images: ["/images/LAD02.png", "/images/LAD02a.png"]
+  },
+  {
+    id: "hello01",
+    name: "HELLO01",
+    category: "niñas",
+    images: ["/images/HELLO01.png", "/images/HELLO01a.png"]
+  },
+  {
+    id: "hello02",
+    name: "HELLO02",
+    category: "niñas",
+    images: ["/images/HELLO02.png", "/images/HELLO02a.png"]
+  },
+  {
+    id: "ela01",
+    name: "ELA01",
+    category: "niñas",
+    images: ["/images/ELA01.png", "/images/ELA01a.png"]
+  },
+  {
+    id: "ela02",
+    name: "ELA02",
+    category: "niñas",
+    images: ["/images/ELA02.png", "/images/ELA02a.png"]
+  },
+  {
+    id: "ela03",
+    name: "ELA03",
+    category: "niñas",
+    images: ["/images/ELA03.png", "/images/ELA03a.png"]
+  },
+  {
+    id: "ela04",
+    name: "ELA04",
+    category: "niñas",
+    images: ["/images/ELA04.jpg", "/images/ELA04a.jpg"]
+  },
+  {
+    id: "ela07",
+    name: "ELA07",
+    category: "niñas",
+    images: ["/images/ELA07.jpg", "/images/ELA07a.jpg"]
+  },
+  {
+    id: "bellof01",
+    name: "BELLOF01",
+    category: "niñas",
+    images: ["/images/BELLOF01.png", "/images/BELLOF01a.png"]
+  },
+  {
+    id: "anif01",
+    name: "ANIF01",
+    category: "niñas",
+    images: ["/images/ANIF01.png", "/images/ANIF01a.png"]
+  },
+  {
+    id: "anif02",
+    name: "ANIF02",
+    category: "niñas",
+    images: ["/images/ANIF02.png", "/images/ANIF02a.png"]
+  },
+  {
+    id: "anif03",
+    name: "ANIF03",
+    category: "niñas",
+    images: ["/images/ANIF03.png", "/images/ANIF03a.png"]
+  },
+  {
+    id: "anif04",
+    name: "ANIF04",
+    category: "niñas",
+    images: ["/images/ANIF04.png", "/images/ANIF04a.png"]
+  },
+  {
+    id: "anif05",
+    name: "ANIF05",
+    category: "niñas",
+    images: ["/images/ANIF05.png", "/images/ANIF05a.png"]
+  },
+  {
+    id: "anif06",
+    name: "ANIF06",
+    category: "niñas",
+    images: ["/images/ANIF06.png", "/images/ANIF06a.png"]
+  },
+  {
+    id: "anif07",
+    name: "ANIF07",
+    category: "niñas",
+    images: ["/images/ANIF07.jpg", "/images/ANIF07a.jpg"]
+  },
+  {
+    id: "an01",
+    name: "AN01",
+    category: "niñas",
+    images: ["/images/AN01.png", "/images/AN01a.png"]
+  },
+  {
+    id: "an02",
+    name: "AN02",
+    category: "niñas",
+    images: ["/images/AN02.png", "/images/AN02a.png"]
+  },
+  {
+    id: "ppf01",
+    name: "PPF01",
+    category: "niñas",
+    images: ["/images/PPF01.png", "/images/PPF01a.png"]
+  },
+  {
+    id: "beba01",
+    name: "BEBA01",
+    category: "niñas",
+    images: ["/images/BEBA01.jpg", "/images/BEBA01a.jpg"]
+  },
+  {
+    id: "bulm01",
+    name: "BULM01",
+    category: "niñas",
+    images: ["/images/BULM01.png", "/images/BULM01a.png"]
+  },
+  {
+    id: "pchef01",
+    name: "PCHF01",
+    category: "niñas",
+    images: ["/images/PCHF01.jpg", "/images/PCHF01a.jpg"]
+  },
+  {
+    id: "an01",
+    name: "AN01",
+    category: "niños",
+    images: ["/images/AN01.png", "/images/AN01a.png", "/images/AN01b.png"]
+  },
+  {
+    id: "an02",
+    name: "AN02",
+    category: "niños",
+    images: ["/images/AN02.png", "/images/AN02a.png"]
+  },
+  {
+    id: "an03",
+    name: "AN03",
+    category: "niños",
+    images: ["/images/AN03.png", "/images/AN03a.png"]
+  },
+  {
+    id: "an04",
+    name: "AN04",
+    category: "niños",
+    images: ["/images/AN04.png", "/images/AN04a.png"]
+  },
+  {
+    id: "an05",
+    name: "AN05",
+    category: "niños",
+    images: ["/images/AN05.png", "/images/AN05a.png"]
+  },
+  {
+    id: "an06",
+    name: "AN06",
+    category: "niños",
+    images: ["/images/AN06.png", "/images/AN06a.png"]
+  },
+  {
+    id: "an07",
+    name: "AN07",
+    category: "niños",
+    images: ["/images/AN07.png", "/images/AN07a.png"]
+  },
+  {
+    id: "an08",
+    name: "AN08",
+    category: "niños",
+    images: ["/images/AN08.png", "/images/AN08a.png"]
+  },
+  {
+    id: "fut02",
+    name: "FUT02",
+    category: "niños",
+    images: ["/images/FUT02.png", "/images/FUT02a.png"]
+  },
+  {
+    id: "gt01",
+    name: "GT01",
+    category: "niños",
+    images: ["/images/GT01.png", "/images/GT01a.png"]
+  },
+  {
+    id: "gz01",
+    name: "GZ01",
+    category: "niños",
+    images: ["/images/GZ01.png", "/images/GZ01a.png"]
+  },
+  {
+    id: "mico1",
+    name: "MICO1",
+    category: "niños",
+    images: ["/images/MIC01.png", "/images/MIC01a.png"]
+  },
+  {
+    id: "mico2",
+    name: "MICO2",
+    category: "niños",
+    images: ["/images/MIC02.png", "/images/MIC02a.png"]
+  },
+  {
+    id: "mico3",
+    name: "MICO3",
+    category: "niños",
+    images: ["/images/MIC03.png", "/images/MIC03a.png"]
+  },
+  {
+    id: "mico4",
+    name: "MICO4",
+    category: "niños",
+    images: ["/images/MIC04.png", "/images/MIC04a.png"]
+  },
+  {
+    id: "mico5",
+    name: "MICO5",
+    category: "niños",
+    images: ["/images/MIC05.png", "/images/MIC05a.png"]
+  },
+  {
+    id: "mico6",
+    name: "MICO6",
+    category: "niños",
+    images: ["/images/MIC06.png", "/images/MIC06a.png"]
+  },
+  {
+    id: "mico7",
+    name: "MICO7",
+    category: "niños",
+    images: ["/images/MIC07.png", "/images/MIC07a.png"]
+  },
+  {
+    id: "mico8",
+    name: "MICO8",
+    category: "niños",
+    images: ["/images/MIC08.png", "/images/MIC08a.png"]
+  },
+  {
+    id: "mico9",
+    name: "MICO9",
+    category: "niños",
+    images: ["/images/MIC09.png", "/images/MIC09a.png"]
+  },
+  {
+    id: "mico10",
+    name: "MICO10",
+    category: "niños",
+    images: ["/images/MIC10.png", "/images/MIC10a.png"]
+  },
+  {
+    id: "mico11",
+    name: "MICO11",
+    category: "niños",
+    images: ["/images/MIC11.jpg", "/images/MIC11a.jpg"]
+  },
+  {
+    id: "mndo1",
+    name: "MNO1",
+    category: "niños",
+    images: ["/images/MN01.png", "/images/MN01a.png"]
+  },
+  {
+    id: "op01",
+    name: "OP01",
+    category: "niños",
+    images: ["/images/OP01.jpg", "/images/OP01a.jpg"]
+  },
+  {
+    id: "pato1",
+    name: "PATO1",
+    category: "niños",
+    images: ["/images/PAT01.png", "/images/PAT01a.png"]
+  },
+  {
+    id: "pch01",
+    name: "PCH01",
+    category: "niños",
+    images: ["/images/PCH01.png", "/images/PCH01a.png"]
+  },
+  {
+    id: "plimo1",
+    name: "PLIMO1",
+    category: "niños",
+    images: ["/images/PLIM01.jpg", "/images/PLIM01a.jpg"]
+  },
+  {
+    id: "poco1",
+    name: "POCO1",
+    category: "niños",
+    images: ["/images/POC01.png", "/images/POC01a.png"]
+  },
+  {
+    id: "pp01",
+    name: "PP01",
+    category: "niños",
+    images: ["/images/PP01.png", "/images/PP01a.png"]
+  },
+  {
+    id: "pp02",
+    name: "PP02",
+    category: "niños",
+    images: ["/images/PP02.png", "/images/PP02a.png"]
+  },
+  {
+    id: "pp03",
+    name: "PP03",
+    category: "niños",
+    images: ["/images/PP03.png", "/images/PP03a.png"]
+  },
+  {
+    id: "pri01",
+    name: "PRI01",
+    category: "niños",
+    images: ["/images/PRI01.jpg", "/images/PRI01a.jpg"]
+  },
+  {
+    id: "pw01",
+    name: "PW01",
+    category: "niños",
+    images: ["/images/PW01.png", "/images/PW01a.png"]
+  },
+  {
+    id: "rg01",
+    name: "RG01",
+    category: "niños",
+    images: ["/images/RG01.png", "/images/RG01a.png"]
+  },
+  {
+    id: "rg02",
+    name: "RG02",
+    category: "niños",
+    images: ["/images/RG02.png", "/images/RG02a.png"]
+  },
+  {
+    id: "sco1",
+    name: "SC01",
+    category: "niños",
+    images: ["/images/SC01.png", "/images/SC01a.jpg"]
+  },
+  {
+    id: "shr01",
+    name: "SHR01",
+    category: "niños",
+    images: ["/images/SHR01.png", "/images/SHR01a.png"]
+  },
+  {
+    id: "simo1",
+    name: "SIMO1",
+    category: "niños",
+    images: ["/images/SIM01.png", "/images/SIM01a.jpg"]
+  },
+  {
+    id: "slo1",
+    name: "SLO1",
+    category: "niños",
+    images: ["/images/SL01.png", "/images/SL01a.jpg"]
+  },
+
+  // --- SN son productos separados ---
+  {
+    id: "sn1",
+    name: "SN1",
+    category: "niños",
+    images: ["/images/sn1.png"]
+  },
+  {
+    id: "sn4",
+    name: "SN4",
+    category: "niños",
+    images: ["/images/sn4.png"]
+  },
+  {
+    id: "sn7",
+    name: "SN7",
+    category: "niños",
+    images: ["/images/sn7.png"]
+  },
+  {
+    id: "sn8",
+    name: "SN8",
+    category: "niños",
+    images: ["/images/sn8.png"]
+  },
+  {
+    id: "sn9",
+    name: "SN9",
+    category: "niños",
+    images: ["/images/sn9.png"]
+  },
+  {
+    id: "sn19",
+    name: "SN19",
+    category: "niños",
+    images: ["/images/sn19.png"]
+  },
+
+  {
+    id: "sp01",
+    name: "SP01",
+    category: "niños",
+    images: ["/images/SP01.png", "/images/SP01a.png"]
+  },
+  {
+    id: "stcho1",
+    name: "STCHO1",
+    category: "niños",
+    images: ["/images/STCH01.jpg", "/images/STCH01a.jpg"]
+  },
+  {
+    id: "stcho2",
+    name: "STCHO2",
+    category: "niños",
+    images: ["/images/STCH02.png", "/images/STCH02a.jpg"]
+  },
+  {
+    id: "tr01",
+    name: "TR01",
+    category: "niños",
+    images: ["/images/TR01.png", "/images/TR01a.png"]
+  },
+  {
+    id: "ts01",
+    name: "TS01",
+    category: "niños",
+    images: ["/images/TS01.png", "/images/TS01a.png"]
+  },
+  {
+    id: "ts02",
+    name: "TS02",
+    category: "niños",
+    images: ["/images/TS02.png", "/images/TS02a.png"]
+  },
+  {
+    id: "ts03",
+    name: "TS03",
+    category: "niños",
+    images: ["/images/TS03.png", "/images/TS03a.png"]
+  },
+  {
+    id: "tt01",
+    name: "TT01",
+    category: "niños",
+    images: ["/images/TT01.jpg", "/images/TT01a.jpg"]
+  },
+  {
+    id: "vf01",
+    name: "VF01",
+    category: "niños",
+    images: ["/images/VF01.png", "/images/VF01a.png"]
+  },
+  {
+    id: "wp01",
+    name: "WP01",
+    category: "niños",
+    images: ["/images/WP01.png", "/images/WP01a.png"]
+  },
+  {
+    id: "wp02",
+    name: "WP02",
+    category: "niños",
+    images: ["/images/WP02.png", "/images/WP02a.png"]
+  },
+  {
+    id: "wp03",
+    name: "WP03",
+    category: "niños",
+    images: ["/images/WP03.png", "/images/WP03a.png"]
+  },
+  {
+    id: "wp04",
+    name: "WP04",
+    category: "niños",
+    images: ["/images/WP04.png", "/images/WP04a.png"]
+  },
+  {
+    id: "wp05",
+    name: "WP05",
+    category: "niños",
+    images: ["/images/WP05.png", "/images/WP05a.png"]
+  },
+  {
+    id: "zn01",
+    name: "ZN01",
+    category: "niños",
+    images: ["/images/ZN01.png", "/images/ZN01a.png"]
+  }
+];
+export default products;
